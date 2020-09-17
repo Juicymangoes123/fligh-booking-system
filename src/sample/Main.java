596d1147e96fc1eb41ff3941ec837bb73dfe5241
Randil <randilhetti@gmail.com>
2020-09-17 19:00:15 +1000

    New layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("login_page.fxml"));
+        primaryStage.setTitle("Flight Booking System");
+        primaryStage.setScene(new Scene(root, 600, 400));
         primaryStage.show();
     }
 
